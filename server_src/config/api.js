bebb6296c68aaa52c6e5a42eaaaa14f2ad7b91b1
ltran <longdtranw@gmail.com>
2023-03-27 17:56:16 -0600

    got it to store the destination file as a temporary file

--- server_src/config/api.js
+++ server_src/config/api.js
@@ -1,26 +1,44 @@
 const { exec } = require("child_process");
 const fs = require('fs');
+const tmp = require('tmp');
 
 module.exports = function (app) {
-    app.route('/run_example').get(runExample)    
+    app.route('/run_example').get(runExample);    
+    app.route('/extract').post(extractRequestHandler);
 }
 
 function runExample(req, res) {
- 
-  exec("python3 ../core/extract.py --src ../cps.owl --des ../truncated.owl", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-    }
 
-    // attempt to read from file
-   
+    const desSrc = tmp.fileSync();
+    desPath = desSrc.name
+
+    exec(`python3 ../core/extract.py --src ../cps.owl --des "${desPath}"`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            res.error("An error occured while parsing the data");
+            desSrc.removeCallback();
+            return;
+        }  // end if
         
-    // return the file
-    console.log(`stdout: ${stdout}`);
-    res.set('Content-Type', 'text/html');
-    res.download("../truncated.owl");
-  });
-}
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+        } // end if
+
+        // return the file
+        console.log(`stdout: ${stdout}`);
+        res.download("../truncated.owl", filename="truncated.owl", (err) => {
+            if (err)
+                console.log("error: " + error);
+            else 
+                console.log("file removed");
+            
+            // unlink the temporary file
+            desSrc.removeCallback();
+        }); // end res.download
+    });
+    
+} // end runExample
+
+function extractRequestHandler(req, res) {
+
+} // end extractRequestHandler
